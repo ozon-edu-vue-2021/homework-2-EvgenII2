0db84bfd7f8d16e02cd51473d055ff9bec16baab
EvgenII L <genja1988@inbox.ru>
2021-10-27 16:16:41 +0300

    added component tree

---
+++ src/components/Folder.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="tree-folder">
+    <p class="tree-folder__title" v-on:click="clickHandler">
+      {{ "DIRECTORY: " + name }}
+    </p>
+    <ul class="tree-folder__content">
+      <li v-for="(item, index) in contents" :key="index" v-show="visible">
+        <!-- Мне не нравится, что каждый элемент проверяется на isFolder, isFile, isLink -->
+        <Folder
+          v-if="isFolder(item)"
+          :name="item.name"
+          :contents="item.contents"
+        />
+        <File v-if="isFile(item)" :name="item.name" />
+        <Link v-if="isLink(item)" :name="item.name" :target="item.target" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { isFolder, isFile, isLink } from "../utils/utils";
+import File from "./File.vue";
+import Link from "./Link.vue";
+
+export default {
+  name: "Folder",
+  components: {
+    File,
+    Link,
+  },
+  data() {
+    return {
+      visible: false,
+    };
+  },
+  props: {
+    name: {
+      type: String,
+    },
+    contents: {
+      type: Array,
+    },
+  },
+  methods: {
+    clickHandler() {
+      this.visible = !this.visible;
+    },
+    isFolder,
+    isFile,
+    isLink,
+  },
+};
+</script>
+
+<style scoped>
+.tree-folder__content {
+  margin: 0;
+  list-style: none;
+}
+.tree-folder__title {
+  cursor: pointer;
+  color: blue;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/Tree.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="tree">
+    <Folder :name="items.name" :contents="items.contents" />
+  </div>
+</template>
+
+<script>
+import items from "../../public/static/node_modules.json";
+import Folder from "./Folder.vue";
+export default {
+  name: "Tree",
+  components: {
+    Folder,
+  },
+  data: () => ({
+    items,
+  }),
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
